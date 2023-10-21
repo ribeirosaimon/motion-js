007c908c4e9d0dda3d89c607d4e3593211143bef
Saimon <saimonribeiros@hotmail.com>
2023-10-21 13:06:41 -0300

    balance

--- src/app/pages/navBar/SocialNavBar/Balance.jsx
+++ src/app/pages/navBar/SocialNavBar/Balance.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import {useEffect, useState} from "react";
+import {HttpGetAxios} from "../../../utils/HttpBasicAxios";
+import {formatCurrency} from "../../../components/util/NumberUtil";
 
 const BalanceInfo = styled.div`
   margin-top: 30px;
@@ -8,9 +11,17 @@ const BalanceInfo = styled.div`
 
 
 const Balance = () => {
+    const [balance, setBalance] = useState(0)
+    useEffect(() => {
+        HttpGetAxios("/transaction/balance")
+            .then(r => {
+                setBalance(r.data.value)
+            })
+    },[])
+
     return(
         <BalanceInfo>
-            BALANCE
+            {formatCurrency(balance)}
         </BalanceInfo>
     )
 }
